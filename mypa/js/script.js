57d7d37943ff95c25437f3435a668508171b7b2c
bharatborikar <borikar.bharat@gmail.com>
2017-03-24 00:13:30 +0530

    Update script.js

--- mypa/js/script.js
+++ mypa/js/script.js
@@ -74,7 +74,7 @@ function make_listen(text)
 		console.log("Speech Recognisation is not working!");  
 	}
 	recognizer = new SpeechRecognition();
-	recognizer.continuous = false;
+	recognizer.continuous = true;
 	recognizer.interimResults = false; 
 	recognizer.addEventListener('result', function(event) {
                transcription = '';
